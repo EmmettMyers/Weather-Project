447ce454259d65937a013198031d2fb4bb5e189b
Emmett Myers <emmettleemyers@gmail.com>
2024-04-26 17:14:02 -0500

    Improvements

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { GiPaperWindmill } from "react-icons/gi";
 
 function App() {
   const [inputs, setInputs] = useState({});
-  const [prediction, setPrediction] = useState("");
+  const [predictions, setPredictions] = useState([]);
 
   const handleChange = (event) => {
     const name = event.target.name;
@@ -27,133 +27,141 @@ function App() {
       body: JSON.stringify(inputs),
     });
     const data = await response.json();
-    setPrediction(data.prediction);
+    setPredictions(data.predictions);
   };
 
   return (
     <div className="App">
-      <form className="model-form" onSubmit={handleSubmit}>
-        <h1>Enter Your Values Below</h1>
-        <div className="underline-container">
-          <div class="label-input-container">
-            <div class="icon-label-container">
-              <label className="feature-label" htmlFor="temperature">
-                Temperature
-              </label>
-              <FaTemperatureQuarter />
+      <div className="project-holder">
+        <div className="project-title">Using Current U.S. Weather Features to Predict Location</div>
+        <div className="cards-container">
+          <form className="model-form" onSubmit={handleSubmit}>
+            <h1 className="enter-txt">Enter Weather Features</h1>
+            <div className="underline-container">
+              <div className="label-input-container">
+                <div className="icon-label-container">
+                  <label className="feature-label" htmlFor="temperature">
+                    Temperature (&deg;F)
+                  </label>
+                  <FaTemperatureQuarter />
+                </div>
+                <input
+                  required
+                  id="temperature"
+                  name="temperature"
+                  value={inputs.temperature || ""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="underline" />
             </div>
-            <input
-              required
-              id="temperature"
-              name="temperature"
-              value={inputs.temperature || ""}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="underline" />
-        </div>
-        <div className="underline-container">
-          <div class="label-input-container">
-            <div class="icon-label-container">
-              <label className="feature-label" htmlFor="humidity">
-                Humidity
-              </label>
-              <FaDroplet />
+            <div className="underline-container">
+              <div className="label-input-container">
+                <div className="icon-label-container">
+                  <label className="feature-label" htmlFor="humidity">
+                    Humidity (%)
+                  </label>
+                  <FaDroplet />
+                </div>
+                <input
+                  required
+                  id="humidity"
+                  name="humidity"
+                  value={inputs.humidity || ""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="underline" />
             </div>
-            <input
-              required
-              id="humidity"
-              name="humidity"
-              value={inputs.humidity || ""}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="underline" />
-        </div>
 
-        <div className="underline-container">
-          <div class="label-input-container">
-            <div class="icon-label-container">
-              <label className="feature-label" htmlFor="dewPoint">
-                Dew Point
-              </label>
-              <FaTemperatureLow />
+            <div className="underline-container">
+              <div className="label-input-container">
+                <div className="icon-label-container">
+                  <label className="feature-label" htmlFor="dewPoint">
+                    Dew Point (&deg;F)
+                  </label>
+                  <FaTemperatureLow />
+                </div>
+                <input
+                  required
+                  id="dewPoint"
+                  name="dewPoint"
+                  value={inputs.dewPoint || ""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="underline" />
             </div>
-            <input
-              required
-              id="dewPoint"
-              name="dewPoint"
-              value={inputs.dewPoint || ""}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="underline" />
-        </div>
 
-        <div className="underline-container">
-          <div class="label-input-container">
-            <div class="icon-label-container">
-              <label className="feature-label" htmlFor="precipitation">
-                Precipitation
-              </label>
-              <IoRainyOutline />
+            <div className="underline-container">
+              <div className="label-input-container">
+                <div className="icon-label-container">
+                  <label className="feature-label" htmlFor="precipitation">
+                    Precipitation (in)
+                  </label>
+                  <IoRainyOutline />
+                </div>
+                <input
+                  required
+                  id="precipitation"
+                  name="precipitation"
+                  value={inputs.precipitation || ""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="underline" />
             </div>
-            <input
-              required
-              id="precipitation"
-              name="precipitation"
-              value={inputs.precipitation || ""}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="underline" />
-        </div>
 
-        <div className="underline-container">
-          <div class="label-input-container">
-            <div class="icon-label-container">
-              <label className="feature-label" htmlFor="windSpeed">
-                Wind Speed
-              </label>
-              <FaWind />
+            <div className="underline-container">
+              <div className="label-input-container">
+                <div className="icon-label-container">
+                  <label className="feature-label" htmlFor="windSpeed">
+                    Wind Speed (kt)
+                  </label>
+                  <FaWind />
+                </div>
+                <input
+                  required
+                  id="windSpeed"
+                  name="windSpeed"
+                  value={inputs.windSpeed || ""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="underline" />
             </div>
-            <input
-              required
-              id="windSpeed"
-              name="windSpeed"
-              value={inputs.windSpeed || ""}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="underline" />
-        </div>
 
-        <div className="underline-container">
-          <div class="label-input-container">
-            <div class="icon-label-container">
-              <label className="feature-label" htmlFor="windDirection">
-                Wind Direction
-              </label>
-              <GiPaperWindmill />
+            <div className="underline-container">
+              <div className="label-input-container">
+                <div className="icon-label-container">
+                  <label className="feature-label" htmlFor="windDirection">
+                    Wind Direction (&deg;)
+                  </label>
+                  <GiPaperWindmill />
+                </div>
+                <input
+                  required
+                  id="windDirection"
+                  name="windDirection"
+                  value={inputs.windDirection || ""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="underline" />
             </div>
-            <input
-              required
-              id="windDirection"
-              name="windDirection"
-              value={inputs.windDirection || ""}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="underline" />
-        </div>
 
-        <button className="predict-button" type="submit">
-          Predict
-        </button>
-      </form>
-      <div class="predictions-container">
-        <h1>Predictions</h1>
-        {prediction && <p className="prediction"> {prediction}</p>}
+            <button className="predict-button" type="submit">
+              Predict
+            </button>
+          </form>
+          <div className="predictions-container">
+            <h1 className="predicted-title">Predicted Cities</h1>
+            <div className="predicted-desc">Ranked by predicted percentage, or how close the entered features are to a city's own features</div>
+            {predictions.map((prediction, index) => (
+              <p key={index} className="prediction">{`${index + 1}. ${prediction}`}</p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
